95bf02feed0ec72458501aa3e5cf6d4a0276f36f
Carlos Gutierrez <cgutierrez@utp.edu.co>
2023-01-27 14:54:01 -0500

    add categorias migration

---
+++ app/Database/Migrations/2023-01-27-065131_AddCategorias.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddCategorias extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nombre' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'datetime',
+                'null' => true,
+            ],
+            'created_at datetime default current_timestamp',
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('categorias');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('categorias');
+    }
+}
