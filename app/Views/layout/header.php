ab5c61467cbd5e7f7143487ecf65212294d68fc8
Carlos Gutierrez <cgutierrez@utp.edu.co>
2023-01-27 14:53:41 -0500

    add layout for admintLTE

---
+++ app/Views/layout/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title><?= 'CoffeeShop - ' . esc($title) ?></title>
+
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= csrf_meta()?>
+
+    <!-- Google Font: Thai Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="<?= base_url('asset/plugins/fontawesome-free/css/all.min.css') ?>">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="<?= base_url('asset/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url('asset/css/adminlte.min.css') ?>">
+     <!--<link rel="stylesheet" href="<?php /* echo base_url('asset/css/rtl/adminlte.rtl.min.css') */ ?>"> --> 
+	 
+    <!-- SweetAlert2 Bootstrap or Dark -->
+    <link rel="stylesheet" href="<?= base_url('asset/css/sweetalert2-dark.min.css') ?>">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- DataTables -->
+ 	<link rel="stylesheet" href="<?= base_url('asset/plugins/datatables/DataTables-1.11.3/css/dataTables.bootstrap5.min.css') ;?>">
+	<link rel="stylesheet" href="<?= base_url('asset/plugins/datatables/Responsive-2.2.9/css/responsive.bootstrap5.min.css') ;?>"> 
+
+    <link rel="stylesheet" href="<?= base_url('asset/plugins/datatables/StateRestore-1.1.1/css/stateRestore.bootstrap5.min.css') ?>">
+    <!-- Dark style -->
+    <!--<link rel="stylesheet" href="<?php /* echo base_url('asset/css/dark/adminlte-dark-addon.min.css')*/ ?>">  --> 
+</head>
---
+++ app/Views/layout/sidebar.php
@@ -0,0 +1,51 @@
+      <!-- Main Sidebar Container -->
+<!--<aside class="main-sidebar sidebar-bg-dark sidebar-color-primary shadow">-->
+<aside class="main-sidebar sidebar-bg-light sidebar-color-primary shadow">
+  <div class="brand-container">
+    <a href="javascript:;" class="brand-link">
+      <img src="<?= base_url('asset/img/AdminLTELogo.png') ?>" alt="AdminLTE Logo" class="brand-image opacity-80 shadow">
+      <span class="brand-text fw-light">AdminLTE 4</span>
+    </a>
+    <a class="pushmenu mx-1" data-lte-toggle="sidebar-mini" href="javascript:;" role="button"><i class="fas fa-angle-double-left"></i></a>
+  </div>
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <nav class="mt-2">
+      <!-- Sidebar Menu -->
+      <ul class="nav nav-pills nav-sidebar flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+        <li class="nav-item menu-open">
+          <a href="javascript:;" class="nav-link active">
+            <i class="nav-icon fas fa-circle"></i>
+            <p>
+              Dashboard
+              <i class="end fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              
+              <a href="<?= base_url('productos') ?>" class="nav-link active">
+                <i class="nav-icon far fa-circle"></i>
+                <p>Productos</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= base_url('categorias') ?>" class="nav-link ">
+                <i class="nav-icon far fa-circle"></i>
+                <p>Categorias</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="<?= base_url('ventas') ?>"" class="nav-link ">
+                <i class="nav-icon far fa-circle"></i>
+                <p>Ventas</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+
+      </ul>
+    </nav>
+  </div>
+  <!-- /.sidebar -->
+</aside>
