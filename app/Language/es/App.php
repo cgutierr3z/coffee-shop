ebe677e65f16b87ba22df9db5f25fc07b976f6e9
Carlos Gutierrez <cgutierrez@utp.edu.co>
2023-01-27 14:54:54 -0500

    add locales en, es

---
+++ app/Language/es/App.php
@@ -0,0 +1,23 @@
+<?php
+
+
+return [
+    'new' => 'Nuevo',
+    'add'  => 'Agregar',
+    'edit'  => 'Editar',
+    'update' => 'Actualizar',
+    'delete' => 'Eliminar',
+    'copy' => 'Duplicar',
+    'save'  => 'Guardar',
+    'confirm' => 'Confirmar',
+    'cancel'  => 'Cancelar',
+    'remove-title' => '¿Seguro desea eliminar?',
+    'remove-text' => 'No se puede deshacer despues de confirmar.',
+    'insert-success' => 'La informacion ha sido guardada.',
+    'insert-error' => 'Error al guardar!',
+    'update-success' => 'Actualizado correctamente.',
+    'update-error' => 'Error de actualizacion!',
+    'delete-success' => 'Eliminado correctamente.',
+    'delete-error' => 'Error al eliminar!',
+ 
+];
